838bfac4d2a542efbee3b07aceddbe1693d00e03
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-04-05 16:41:38 -0500

    Added homework 3 structure

---
+++ homework3/src/main/java/com/mycompany/homework3/FileShare.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.homework3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author chang
+ */
+public class FileShare {
+    
+    public static void main(String[] args) {
+     int option = Integer.parseInt(JOptionPane.showInputDialog("Bienvenido\n ¿Qué deseas hacer? \n\n"
+            + "1. Crear claves.\n"
+            + "2. Cifrar archivo.\n"
+            + "3. Descifrar archivo."));   
+    }
+    
+    void creaClaves() {
+        
+    }
+    
+    void cifrarArchivo() {
+        
+    }
+    
+    void descifrarArchivo() {
+        
+    }
+}

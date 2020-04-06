77915017a3127f582c1731d8134522779fca788a
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-04-05 23:17:31 -0500

    Finished encrypt and decrypt methods

--- homework3/src/main/java/com/mycompany/homework3/FileShare.java
+++ homework3/src/main/java/com/mycompany/homework3/FileShare.java
@@ -7,6 +7,9 @@ package com.mycompany.homework3;
 
 import java.io.IOException;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
@@ -14,45 +17,77 @@ import javax.swing.JOptionPane;
  * @author chang
  */
 public class FileShare {
-    
-    static final String PUBLIC_KEY_NAME = "keys/rsa.pem";
-    static final String PRIVATE_KEY_NAME = "keys/rsa.pri";
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException, Exception {
 
         RSA rsa = new RSA();
+        int option;
+        do {
+            option = Integer.parseInt(JOptionPane.showInputDialog("Bienvenido\n ¿Qué deseas hacer? \n\n"
+                    + "1. Crear claves.\n"
+                    + "2. Cifrar archivo.\n"
+                    + "3. Descifrar archivo.\n"
+                    + "4. Salir."));
+            switch (option) {
+                case 1:
+                    creaClaves(rsa);
+                    break;
+                case 2:
+                    cifrarArchivo(rsa);
+                    break;
+                case 3:
+                    descifrarArchivo(rsa);
+                    break;
+            }
+        } while (option != 0);
 
-        int option = Integer.parseInt(JOptionPane.showInputDialog("Bienvenido\n ¿Qué deseas hacer? \n\n"
-                + "1. Crear claves.\n"
-                + "2. Cifrar archivo.\n"
-                + "3. Descifrar archivo."));
-        switch (option) {
-            case 1:
-                creaClaves(rsa);
-                break;
-            case 2:
-                cifrarArchivo(rsa);
-                break;
-            case 3:
-                descifrarArchivo(rsa);
-                break;
-        }
     }
 
     static void creaClaves(RSA rsa) throws Exception {
         rsa.genKeyPair(1024);
         //Las guardamos asi podemos usarlas despues
         //a lo largo del tiempo
-        rsa.saveToDiskPrivateKey(PRIVATE_KEY_NAME);
-        String publicKeyPath = rsa.saveToDiskPublicKey(PUBLIC_KEY_NAME);
-        JOptionPane.showMessageDialog(null, "Claves generadas en el directorio:\n" + publicKeyPath.substring(0, publicKeyPath.length() - 7));
+        rsa.saveToDiskPrivateKey(RSA.PRIVATE_KEY_PATH);
+        String publicKeyPath = rsa.saveToDiskPublicKey(RSA.PUBLIC_KEY_PATH);
+        JOptionPane.showMessageDialog(null, "Claves generadas en el directorio:\n" + publicKeyPath.substring(0, publicKeyPath.length() - rsa.PUBLIC_KEY_NAME.length()));
     }
 
     static void cifrarArchivo(RSA rsa) throws Exception {
 
+        String rootProjectPath = rsa.getPublicKeyFile().getAbsolutePath().substring(0, rsa.getPublicKeyFile().getAbsolutePath().length() - rsa.PUBLIC_KEY_PATH.length());
+        System.out.println(rootProjectPath);
+        // Create a File Chooser for public key
+        JFileChooser fcPublicKey = new JFileChooser(rsa.getPublicKeyFile().getAbsolutePath().substring(0, rsa.getPublicKeyFile().getAbsolutePath().length() - rsa.PUBLIC_KEY_NAME.length()));
+        fcPublicKey.setDialogTitle("Seleccionar llave pública");
+        //Execute File Choseer for public key
+        fcPublicKey.showOpenDialog(null);
+        rsa.openFromDiskPublicKey(fcPublicKey.getSelectedFile().getAbsolutePath());
+        rsa.setPublicKeyFile(fcPublicKey.getSelectedFile());
+        // Create a File Chooser for file to encrypt
+        JFileChooser fcFileToEncrypt = new JFileChooser(rootProjectPath + "files-to-encrypt-examples");
+        fcFileToEncrypt.setDialogTitle("Seleccionar archivo a encriptar");
+        fcFileToEncrypt.showOpenDialog(null);
+        rsa.setFileToEncrypt(fcFileToEncrypt.getSelectedFile());
+
+        rsa.encrypt();
     }
 
-    static void descifrarArchivo(RSA rsa) {
+    static void descifrarArchivo(RSA rsa) throws Exception {
+        String rootProjectPath = rsa.getPublicKeyFile().getAbsolutePath().substring(0, rsa.getPublicKeyFile().getAbsolutePath().length() - rsa.PUBLIC_KEY_PATH.length());
+        System.out.println(rootProjectPath);
+        // Create a File Chooser for public key
+        JFileChooser fcPrivateKey = new JFileChooser(rsa.getPublicKeyFile().getAbsolutePath().substring(0, rsa.getPublicKeyFile().getAbsolutePath().length() - rsa.PUBLIC_KEY_NAME.length()));
+        fcPrivateKey.setDialogTitle("Seleccionar llave privada");
+        //Execute File Choseer for public key
+        fcPrivateKey.showOpenDialog(null);
+        rsa.openFromDiskPrivateKey(fcPrivateKey.getSelectedFile().getAbsolutePath());
+        rsa.setPrivateKeyFile(fcPrivateKey.getSelectedFile());
+        // Create a File Chooser for file to encrypt
+        JFileChooser fcFileToDecrypt = new JFileChooser(rootProjectPath + "files");
+        fcFileToDecrypt.setDialogTitle("Seleccionar archivo a desencriptar");
+        fcFileToDecrypt.showOpenDialog(null);
+        rsa.setFileToDecrypt(fcFileToDecrypt.getSelectedFile());
 
+        rsa.decrypt();
     }
 }
--- homework3/src/main/java/com/mycompany/homework3/RSA.java
+++ homework3/src/main/java/com/mycompany/homework3/RSA.java
@@ -3,9 +3,11 @@ package com.mycompany.homework3;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -23,6 +25,8 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.Optional;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -30,11 +34,50 @@ import javax.crypto.NoSuchPaddingException;
 
 public class RSA {
 
-    public PrivateKey PrivateKey = null;
-    public PublicKey PublicKey = null;
+    static final String PUBLIC_KEY_NAME = "public.pem";
+    static final String PUBLIC_KEY_PATH = "keys/" + PUBLIC_KEY_NAME;
+    static final String PRIVATE_KEY_NAME = "private.pri";
+    static final String PRIVATE_KEY_PATH = "keys/" + PRIVATE_KEY_NAME;
+    static final String FILE_ENCRYPTED_PATH = "files/file-encrypted";
+    static final String FILE_DECRYPTED_PATH = "files/file-decrypted";
 
-    public RSA() {
+    private PrivateKey privateKey = null;
+    private PublicKey publicKey = null;
+    private File privateKeyFile = new File(PRIVATE_KEY_PATH);
+    private File publicKeyFile = new File(PUBLIC_KEY_PATH);
+    private File fileToEncrypt;
+    private File fileToDecrypt;
 
+    public File getPrivateKeyFile() {
+        return privateKeyFile;
+    }
+
+    public File getPublicKeyFile() {
+        return publicKeyFile;
+    }
+
+    public void setPrivateKeyFile(File privateKeyFile) {
+        this.privateKeyFile = privateKeyFile;
+    }
+
+    public void setPublicKeyFile(File publicKeyFile) {
+        this.publicKeyFile = publicKeyFile;
+    }
+
+    public File getFileToEncrypt() {
+        return fileToEncrypt;
+    }
+
+    public void setFileToEncrypt(File fileToEncrypt) {
+        this.fileToEncrypt = fileToEncrypt;
+    }
+
+    public File getFileToDecrypt() {
+        return fileToDecrypt;
+    }
+
+    public void setFileToDecrypt(File fileToDecrypt) {
+        this.fileToDecrypt = fileToDecrypt;
     }
 
     public void setPrivateKeyString(String key) throws NoSuchAlgorithmException, InvalidKeySpecException {
@@ -42,8 +85,7 @@ public class RSA {
 
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
-        PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
-        this.PrivateKey = privateKey;
+        this.privateKey = keyFactory.generatePrivate(privateKeySpec);
     }
 
     public void setPublicKeyString(String key) throws NoSuchAlgorithmException, InvalidKeySpecException {
@@ -52,17 +94,16 @@ public class RSA {
 
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
-        PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
-        this.PublicKey = publicKey;
+        this.publicKey = keyFactory.generatePublic(publicKeySpec);
     }
 
     public String getPrivateKeyString() {
-        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(this.PrivateKey.getEncoded());
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(this.privateKey.getEncoded());
         return bytesToString(pkcs8EncodedKeySpec.getEncoded());
     }
 
     public String getPublicKeyString() {
-        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(this.PublicKey.getEncoded());
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(this.publicKey.getEncoded());
         return bytesToString(x509EncodedKeySpec.getEncoded());
     }
 
@@ -72,33 +113,43 @@ public class RSA {
         kpg.initialize(size);
         KeyPair kp = kpg.genKeyPair();
 
-        PublicKey publicKey = kp.getPublic();
-        PrivateKey privateKey = kp.getPrivate();
-
-        this.PrivateKey = privateKey;
-        this.PublicKey = publicKey;
+        this.privateKey = kp.getPrivate();
+        this.publicKey = kp.getPublic();
     }
 
-    public String Encrypt(String plain) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, UnsupportedEncodingException, NoSuchProviderException {
-
+    public void encrypt() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, UnsupportedEncodingException, NoSuchProviderException, IOException {
         byte[] encryptedBytes;
 
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, this.PublicKey);
-        encryptedBytes = cipher.doFinal(plain.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, this.publicKey);
+        byte[] fileToEncryptBytes = Files.readAllBytes(fileToEncrypt.toPath());
+        byte[] fileToEncryptBytesEncoded = Base64.getEncoder().encode(fileToEncryptBytes);
+        byte[] fileToEncryptBytesDecoded = Base64.getDecoder().decode(fileToEncryptBytesEncoded);
+        encryptedBytes = cipher.doFinal(fileToEncryptBytesDecoded);
 
-        return bytesToString(encryptedBytes);
-
+        try ( FileOutputStream fos = new FileOutputStream(FILE_ENCRYPTED_PATH + "." + getFileExtension(fileToEncrypt.getAbsolutePath()).get())) {
+            fos.write(encryptedBytes);
+        }
     }
 
-    public String Decrypt(String result) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-
+    public void decrypt() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException {
         byte[] decryptedBytes;
+        
 
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, this.PrivateKey);
-        decryptedBytes = cipher.doFinal(stringToBytes(result));
-        return new String(decryptedBytes);
+        cipher.init(Cipher.DECRYPT_MODE, this.privateKey);
+        byte[] fileToDecryptBytes = Files.readAllBytes(fileToDecrypt.toPath());
+        decryptedBytes = cipher.doFinal(fileToDecryptBytes);
+
+        try ( FileOutputStream fos = new FileOutputStream(FILE_DECRYPTED_PATH + "." + getFileExtension(fileToDecrypt.getAbsolutePath()).get())) {
+            fos.write(decryptedBytes);
+        }
+    }
+
+    public Optional<String> getFileExtension(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
     }
 
     public String bytesToString(byte[] b) {
@@ -114,18 +165,18 @@ public class RSA {
     }
 
     public String saveToDiskPrivateKey(String path) throws IOException {
-        File privateKeyFile = new File(path);
+        this.privateKeyFile = new File(path);
         FileOutputStream fileOutput = new FileOutputStream(privateKeyFile);
         try ( Writer out = new BufferedWriter(new OutputStreamWriter(fileOutput, "UTF-8"))) {
             out.write(this.getPrivateKeyString());
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
     public String saveToDiskPublicKey(String path) throws Exception {
-        File publicKeyFile = new File(path);
+        this.publicKeyFile = new File(path);
         FileOutputStream fileOutput = new FileOutputStream(publicKeyFile);
         try ( Writer out = new BufferedWriter(new OutputStreamWriter(fileOutput, "UTF-8"))) {
             out.write(this.getPublicKeyString());
